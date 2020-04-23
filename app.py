4300ab2484630d2758c38cca31860cbefd88ee96
Michał Kowalski <mkowalski@pgs-soft.com>
2020-04-23 19:56:01 +0200

    added validation of user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request, make_response
 from dblayer import db, User
 
 app = Flask(__name__)
@@ -8,6 +8,17 @@ db.init_app(app)
 @app.route('/api/user', methods=["GET"])
 def getUsers():
     return jsonify([u.serialize for u in User.query.all()])
+
+@app.route('/api/user', methods=["POST"])
+def addUser():
+    jsonObj = request.json
+    error = User.validate(jsonObj)
+    if error:
+        return make_response({'error': error}, 400)
+    userObj = User(jsonObj)
+    db.session.add(userObj)
+    db.session.commit()
+    return make_response({'success':True}, 201)
 
 @app.route('/api/user/<int:user_id>')
 def getUser(user_id):
--- dblayer.py
+++ dblayer.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import escape
 
 db = SQLAlchemy()
 class User(db.Model):
@@ -7,6 +8,21 @@ class User(db.Model):
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     cv_url = db.Column(db.String, unique=True)
+
+    def __init__(self, json):
+        self.first_name = escape(json['first_name'])
+        self.last_name = escape(json['last_name'])
+        self.cv_url = None
+
+    @staticmethod
+    def validate(json):
+        if json == None:
+            return 'missing json object'
+        if 'first_name' not in json:
+            return 'missing first_name'
+        if 'last_name' not in json:
+            return 'missing last_name'
+        return None
 
     @property
     def serialize(self):
